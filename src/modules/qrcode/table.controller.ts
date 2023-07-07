5e2d7e4dce0815e22fc63feba3ae228154e56da8
accela673 <sardar.kasmaliev@alatoo.edu.kg>
2023-07-07 21:02:32 +0600

    finished controllers

--- src/modules/qrcode/table.controller.ts
+++ src/modules/qrcode/table.controller.ts
@@ -1,11 +1,26 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Param, Get } from '@nestjs/common';
 import { TableService } from './table.service';
 import { CreateTableDto } from './dto/create_table.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { BookTableDto } from './dto/book_table.dto';
 
+@ApiTags('Столики с QR кодом')
 @Controller('table')
 export class TableController {
   constructor(private readonly tableService: TableService) {}
 
+  @ApiOperation({ summary: 'Вывести все столики одного филиала' })
+  @Get(':id')
+  async getAllTables(@Param('id') id: number) {
+    return await this.tableService.listAllBranchTables(id);
+  }
+
+  @ApiOperation({ summary: 'Забронировать столик' })
+  @Post()
+  async bookTable(@Body() bookTableDto: BookTableDto) {
+    return await this.tableService.bookOne(bookTableDto);
+  }
+  @ApiOperation({ summary: 'Создать столик с QR кодом' })
   @Post()
   async createTable(@Body() data: CreateTableDto) {
     return await this.tableService.createTable(data);
--- src/modules/qrcode/table.service.ts
+++ src/modules/qrcode/table.service.ts
@@ -6,6 +6,7 @@ import { CreateTableDto } from './dto/create_table.dto';
 import * as qrcode from 'qrcode';
 import { Repository } from 'typeorm';
 import { BranchEntity } from '../branch/entities/branch.entity';
+import { BookTableDto } from './dto/book_table.dto';
 
 @Injectable()
 export class TableService extends BaseService<TableEntity> {
@@ -34,6 +35,39 @@ export class TableService extends BaseService<TableEntity> {
     branch.tables.push(table);
     await this.branchRepo.save(branch);
     return await this.tableRepo.save(table);
+  }
+
+  async bookOne(bookTableDto: BookTableDto) {
+    const { branchId, uniqueCode } = bookTableDto;
+    const table = await this.tableRepo.findOne({
+      where: { uniqueCode: uniqueCode },
+      relations: ['branch'],
+    });
+    const branch = await this.branchRepo.findOne({
+      where: { id: branchId },
+      relations: ['tables'],
+    });
+    if (!table) {
+      throw new BadRequestException('Столик не найден');
+    }
+    if (!branch) {
+      throw new BadRequestException('Филиала с таким id нет');
+    }
+    if (table.branch.id != branch.id) {
+      throw new BadRequestException('Такого столика нет в это филиале');
+    }
+    return 'Столик забронирован';
+  }
+
+  async listAllBranchTables(id: number) {
+    const branch = await this.branchRepo.findOne({
+      where: { id: id },
+      relations: ['tables'],
+    });
+    if (!branch) {
+      throw new BadRequestException('Филиала с таким id не найдено');
+    }
+    return branch.tables;
   }
 
   private async generateRandomString() {
