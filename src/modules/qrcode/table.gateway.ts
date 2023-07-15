5981d9ca55fa4178f2a48cc88ab5f908d69f5b5e
accela673 <sardar.kasmaliev@alatoo.edu.kg>
2023-07-15 17:35:54 +0600

    Fixed gateways

--- src/modules/qrcode/table.gateway.ts
+++ src/modules/qrcode/table.gateway.ts
@@ -4,35 +4,40 @@ import {
   SubscribeMessage,
 } from '@nestjs/websockets';
 import { Server } from 'socket.io';
-import { TablesService } from './tables.service';
-import { BookTableDto } from './dto/book-table.dto';
-import { ReleaseTableDto } from './dto/release-table.dto';
-import { UserRoleEnum } from './user-role.enum';
-import { Roles } from './roles.decorator';
+import { TableService } from './table.service';
+import { BookTableDto } from './dto/book_table.dto';
+import { ReleaseTableDto } from './dto/release_table.dto';
+import { UserRoleEnum } from '../user/enums/user.role.enum';
+import { Roles } from '../auth/roles/role.decorator';
 import { UseGuards } from '@nestjs/common';
-import { JwtAuthGuard } from './jwt-auth.guard';
-import { RoleGuard } from './role.guard';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RoleGuard } from '../auth/roles/role.guard';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Tables')
 @WebSocketGateway()
 export class TablesGateway {
-  constructor(private tablesService: TablesService) {}
+  constructor(private tableService: TableService) {}
 
   @WebSocketServer() server: Server;
 
   @Roles(UserRoleEnum.USER)
   @UseGuards(JwtAuthGuard, RoleGuard)
   @SubscribeMessage('bookTable')
+  @ApiOperation({ summary: 'Забронировать столик' })
+  @ApiBearerAuth()
   async handleBookTable(
     client: any,
     { userId, bookTableDto }: { userId: number; bookTableDto: BookTableDto },
   ): Promise<void> {
-    const table = await this.tablesService.bookTable(userId, bookTableDto);
+    const table = await this.tableService.bookTable(userId, bookTableDto);
     if (table) {
       this.server.emit('tableUpdated', table);
     }
   }
 
   @SubscribeMessage('releaseTable')
+  @ApiOperation({ summary: 'Освободить столик' })
   async handleReleaseTable(
     client: any,
     {
@@ -40,7 +45,7 @@ export class TablesGateway {
       releaseTableDto,
     }: { tableId: number; releaseTableDto: ReleaseTableDto },
   ): Promise<void> {
-    const table = await this.tablesService.releaseTable(
+    const table = await this.tableService.releaseTable(
       tableId,
       releaseTableDto,
     );
