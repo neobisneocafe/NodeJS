111128d4f99afcb8626f7030996c2b94e9d91cb5
Tolgonai022002 <tokaldomotova@gmail.com>
2023-08-09 10:29:32 -0900

    Added array of ingredients

--- src/modules/dishes/dishes.module.ts
+++ src/modules/dishes/dishes.module.ts
@@ -9,16 +9,20 @@ import { ImageModule } from '../image/image.module';
 import { CloudinaryModule } from 'src/services/cloudinary/cloudinary.modules';
 import { Image } from '../image/entities/image.entity';
 import { CategoryService } from '../category/category.service';
+import { MenuItemsModule } from '../menu-items/menu-items.module';
+import { MenuItemsService } from '../menu-items/menu-items.service';
+import { MenuItem } from '../menu-items/entities/menu-item.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Category, Dish, Image]),
+    TypeOrmModule.forFeature([Category, Dish, Image, MenuItem]),
     CategoryModule,
     ImageModule,
     CloudinaryModule,
+    MenuItemsModule,
   ],
   controllers: [DishesController],
-  providers: [DishesService],
+  providers: [DishesService, MenuItemsService],
   exports: [DishesService],
 })
 export class DishesModule {}
